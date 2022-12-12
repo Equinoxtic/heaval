c3c16c038b3294df8078e3b1518a820f20706db4
Equinoxtic <thisisthrealequinoctic@gmail.com>
2022-12-12 18:29:31 +0800

    Update build scripts.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,4 @@
 :: Build script.
 
+powershell -command cmake -G "Visual Studio 17 2022" -A Win32 -S. -Bbuild
 powershell -command cmake --build build --target heavalframework --config release
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+cmake -G "Visual Studio 17 2022" -A Win32 -S. -Bbuild
 cmake --build build --target heavalframework --config release
