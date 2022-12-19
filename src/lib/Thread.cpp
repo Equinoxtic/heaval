c199312f2be91d36d5e0e0dbea6aa603a8167fca
Equinoxtic <thisistherealequinoctic@gmail.com>
2022-12-19 12:28:39 +0800

    Create new Thread library.

---
+++ src/lib/Thread.cpp
@@ -0,0 +1,12 @@
+#include "Thread.h"
+
+/*
+	Auto-Generated C++ file for header: Thread.h with CreateHeader.py
+	Name: Thread
+	Directory: src.lib
+*/
+
+namespace heaval
+{
+	// Class definitions go here...
+}
---
+++ src/lib/Thread.h
@@ -0,0 +1,22 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+/*
+	Auto-Generated header file with CreateHeader.py
+	Name: Thread
+	Directory: src.lib
+*/
+
+namespace heaval
+{
+	class Thread
+	{
+		public:
+			// Public keywords go here...
+			void cSleep(unsigned ms);
+		private:
+			// Private keywords go here...
+	};
+}
+
+#endif // Thread.h
