fe68bc6071c0f712f6a9138b321251f8556807c8
Equinoxtic <thisistherealequinoctic@gmail.com>
2022-12-22 17:49:01 +0800

    Add console Resources

---
+++ src/common/Resources.cpp
@@ -0,0 +1,18 @@
+#include "Resources.h"
+
+/*
+	Auto-Generated C++ file for header: Resources.h with CreateHeader.py
+	Name: Resources
+	Directory: src.common
+*/
+
+namespace heaval
+{
+	// Class definitions go here...
+
+	std::string Resources::VERSION = "0.0.1";
+
+	std::string Resources::BUILD_RELEASE = "ConsBeta - 0.0.1";
+
+	std::string Resources::TITLE = "Heaval Framework [Console]";
+}
