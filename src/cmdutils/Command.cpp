8b753b74fa43df6b288696f84a4c4372d75c7b23
Equinoxtic <thisistherealequinoctic@gmail.com>
2022-12-24 00:06:24 +0800

    Optimize Command library

--- src/cmdutils/Command.cpp
+++ src/cmdutils/Command.cpp
@@ -36,21 +36,11 @@ namespace heaval
 
 	bool Command::checkCommand(std::string baseString, std::string commandString)
 	{
-		bool isCommand = false;
-		if (!StringUtils::stringEmpty(baseString) && !StringUtils::stringEmpty(commandString))
-		{
-			isCommand = (StringUtils::strCompare(baseString, commandString));
-		}
-		return isCommand;
+		return (StringUtils::strCompare(baseString, commandString) ? true : false);
 	}
 
 	bool Command::checkCommandStartsWith(std::string baseString, std::string commandString)
 	{
-		bool isCommand = false;
-		if (!StringUtils::stringEmpty(baseString) && !StringUtils::stringEmpty(commandString))
-		{
-			isCommand = (StringUtils::startsWith(baseString, commandString));
-		}
-		return isCommand;
+		return (StringUtils::startsWith(baseString, commandString) ? true : false);
 	}
 }
