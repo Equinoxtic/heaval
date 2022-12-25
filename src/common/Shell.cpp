a12a6ca73d79da087083f9185d50f1bb2ee74126
Equinoxtic <thisistherealequinoctic@gmail.com>
2022-12-25 14:30:57 +0800

    Update Shell.cpp

--- src/common/Shell.cpp
+++ src/common/Shell.cpp
@@ -7,6 +7,7 @@
 */
 
 #include "lib/StringUtils.h"
+#include "lib/File.h"
 #include "io/Stdio.h"
 
 namespace heaval
@@ -15,7 +16,7 @@ namespace heaval
 
 	std::string Shell::shellUserString = "usr/console";
 
-	std::string Shell::shellType = "standard";
+	std::string Shell::shellType = "custom";
 
 	std::string Shell::createShell(std::string usr, std::string shType)
 	{
@@ -25,6 +26,7 @@ namespace heaval
 		{
 			std::string fShellUser;
 			std::string fShellStringType;
+			std::string configPrefix = "config/shell_";
 
 			if (StringUtils::strCompare(shType, "standard")) {
 				fShellUser = usr;
@@ -35,6 +37,9 @@ namespace heaval
 			} else if (StringUtils::strCompare(shType, "advanced")) {
 				fShellUser = StringUtils::surroundString(usr, "(", ")");
 				fShellStringType = "\n>";
+			} else if (StringUtils::strCompare(shType, "custom")) {
+				fShellUser = File::readFile(configPrefix + "usr.txt");
+				fShellStringType = File::readFile(configPrefix + "type.txt");
 			}
 
 			fShellString = fShellUser + fShellStringType;
