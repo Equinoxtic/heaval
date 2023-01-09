935477b65c274bd29b16248cf5028b4a08b45986
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-09 19:35:20 +0800

    Remove unnecessary functions in buildutils

--- buildutils.h
+++ buildutils.h
@@ -7,7 +7,5 @@ int write_to_file(const char* file_path, const char* file_mode, const char* cont
 int chk_path(const char* path_string);
 bool chk_f(const char* f);
 void exec_cmd(const char* cmd_to_exec);
-int put_case_ch(const char* p_ch, const char* case_mode);
-char* concat_string(const char* s1, const char* s2);
 
 #endif // buildutils.h
